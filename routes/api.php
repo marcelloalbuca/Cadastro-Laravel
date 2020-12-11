1adea10d8aaeda57c59fc86e8c27bc8f518efe1b
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-11 16:53:10 -0300

    Create API, GET and POST

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::prefix('v1')->group(function(){
+    Route::get('lista', function(){
+        return ["a", "b", "c"];
+    });
+
+    Route::post('cadastrar', function(){
+        echo 'implementar';
+    });
 });
+
