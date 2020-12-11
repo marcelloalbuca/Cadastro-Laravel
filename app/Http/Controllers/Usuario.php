95024fd70ec8a508576047b7c74d6750b1aa846f
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-11 16:51:05 -0300

    Controller Usuario, functions cadastrar, salvar

--- app/Http/Controllers/Usuario.php
+++ app/Http/Controllers/Usuario.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class Usuario extends Controller
 {
-    //
+    public function cadastrar(){
+        echo 10;
+    }
+
+    public function salvar(Request $request){
+        dd($request->all());
+    }
 }
