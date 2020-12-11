8e6c2d01ebe7c682838f3265966597352d4efe35
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-11 16:47:25 -0300

    function listar

--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
+    protected $connection = 'mysql';
+    protected $table = 'usuario';
+
+    public static function listar(int $limite){
+        $sql = self::select([
+            "id",
+            "name",
+            "email",
+            "data_cadastro"
+        ])
+        ->limit($limite);
+    
+        dd($sql->toSql());
+    }
 
 }
