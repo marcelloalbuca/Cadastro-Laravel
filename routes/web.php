bdc56ab49df14725948d7047291a52f90f0faf7a
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-11 16:52:02 -0300

    Routes get, post

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Usuario@cadastrar');
+Route::post('/salvar', 'Usuario@salvar');
+
+//Route::match(['get','post'], '/', ); 
