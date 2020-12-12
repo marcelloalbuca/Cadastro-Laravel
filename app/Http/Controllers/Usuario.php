7fa8a19865ddeffe03bcf55f19641542d6b4b50b
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-12 01:05:21 -0300

    return view cadastro

--- app/Http/Controllers/Usuario.php
+++ app/Http/Controllers/Usuario.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Hash;
 
 class Usuario extends Controller
 {
     public function cadastrar(){
-        echo 10;
+        //dd(Hash::make('123'), md5('123'), sha1('123'));
+        return view('usuario.cadastro');
     }
 
     public function salvar(Request $request){
