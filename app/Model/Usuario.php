33b6695a7dfb4881b979af2156af46fd5af690a9
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-14 17:11:31 -0300

    utilizando Carbon e Querylog

--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -2,9 +2,11 @@
 
 namespace App\Model;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Hash;
-
 class Usuario extends Model
 {
     protected $connection = 'mysql';
@@ -19,18 +21,22 @@ class Usuario extends Model
             "data_cadastro"
         ])
         ->limit($limite);
-    
-        dd($sql->toSql());
+
+        //dd($sql->toSql());
+
+        return $sql->get();
     }
 
     public static function cadastrar(Request $request){
-        $sql = self::insert([
+        //DB::enableQueryLog();
+
+       return self::insert([
             "nome" => $request->input('nome'),
             "email" => $request->input('email'),
             "senha" => Hash::make($request->input('senha')),
-            "data_cadastro" => DB::raw('NOW()')
+            "data_cadastro" => new Carbon()
         ]);
 
-        dd($sql->toSql(), $request->all());
+        //dd(DB::getQueryLog());
     }
 }
