259bf749eb286f068191340b42a75ccf9c51fe04
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-15 08:13:04 -0300

    add boostrap, js, jquery

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }} :: @yield('titulo')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap/css/bootstrap.min.css') }}">
+
+    <script type="text/javascript" src="{{ asset('js/jquery-3.5.0.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+
 </head>
 <body>
 
@@ -16,6 +21,7 @@
     <div class="container">
         @yield('conteudo')
     </div>
+
 
     <footer>
         <center><p>Marcello Albuquerque</p></center>
