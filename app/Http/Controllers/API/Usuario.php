17ff8ce30a218ca564239c7d5ba520bdb893dfa1
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2020-12-15 08:12:16 -0300

    function cadastrar API

--- app/Http/Controllers/API/Usuario.php
+++ app/Http/Controllers/API/Usuario.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Usuario as UsuarioModel;
 
 class Usuario extends Controller
 {
     public function salvar(Request $request){
-        dd($request->all());
+        //dd($request->all());
+
+        if(UsuarioModel::cadastrar($request)){
+            return response("ok", 201);
+        } else {
+            return response("error", 409);
+        }  
     }
 }
